c52acc17356c4c540153fe77e1a462b7a5f75614
Renzo Gabriel <renzo.gabrielch@gmail.com>
2025-06-13 19:42:01 +0200

    Último cambio

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/PortafolioModerno", // 👈 AGREGA ESTA LÍNEA
-
   server: {
     host: "::",
     port: 8080,
